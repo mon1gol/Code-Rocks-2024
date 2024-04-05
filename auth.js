bae843adcd00dee5889baf1ce335443056df8387
mon1gol <profile4634@yandex.ru>
2024-04-05 19:36:38 +0300

    теперь переводит на главную страницу

--- auth.js
+++ auth.js
@@ -1,19 +1,17 @@
-function regCheckForm(){
-    var el = document.getElementById("main-form");
-
+function regCheckForm(el){
     var name = el.name.value;
     var email = el.email.value;
     var pass = el.pass.value;
     var repass = el.repass.value;
 
     var fail = "";
-    if (name == "" || pass == "" || repass == "" || state == "") {
+    if (name == "" || email == "" || pass == "" || repass == "") {
         fail = "Заполните все поля";
     }
     else if (name.length <= 2 || name.length >= 25) {
         fail = "Введите корректное имя";
     }
-    else if(email.split("@") < 1){
+    else if(email.split("@") < 1 || email.split("@") >= 2){
         fail = "Email введен некорректно";
     }
     else if (pass != repass) {
@@ -25,28 +23,28 @@ function regCheckForm(){
 
     if (fail != "") {
         document.getElementById('error').innerHTML = fail;
-        return false;
     } else {
         alert("данные введены корректно");
         window.location = "http://127.0.0.1:5500/index.html";
     }
+    return false;
 }
 
 
-function loginCheckForm(){
+function loginCheckForm(el){
     var name = el.name.value;
     var pass = el.pass.value;
 
     var fail = ""
-    if (name == "" || pass == "" || repass == "" || state == "") {
+    if (name == "" || pass == "") {
         fail = "Заполните все поля";
     }
 
     if (fail != "") {
         document.getElementById('error').innerHTML = fail;
-        return false;
     } else{
         alert("данные введены корректно");
         window.location = "http://127.0.0.1:5500/index.html";
     }
+    return false;
 }
